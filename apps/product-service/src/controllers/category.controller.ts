61b624b45ec2d80315d294863a8a2f89a446a1d6
Agostnho F  Teles Jr. <atelesjr@gmail.com>
2026-02-20 18:26:17 -0300

    products: category controllers: getCategories, updateCategory, deleteCategory

--- apps/product-service/src/controllers/category.controller.ts
+++ apps/product-service/src/controllers/category.controller.ts
@@ -8,9 +8,26 @@ export const createCategory = async (req: Request, res: Response) => {
 	res.status(201).json(category);
 };
 
-export const updateCategory = async (req: Request, res: Response) => {};
+export const updateCategory = async (req: Request, res: Response) => {
+	const { id } = req.params;
+	const data: Prisma.CategoryUpdateInput = req.body;
 
-export const deleteCategory = async (req: Request, res: Response) => {};
+	const updatedCategory = await prisma.category.update({
+		where: { id: Number(id) },
+		data,
+	});
+
+	res.status(200).json(updatedCategory);
+};
+
+export const deleteCategory = async (req: Request, res: Response) => {
+	const { id } = req.params;
+
+	const deletedCategory = await prisma.category.delete({
+		where: { id: Number(id) },
+	});
+	res.status(200).json(deletedCategory);
+};
 
 export const getCategories = async (req: Request, res: Response) => {
 	const categories = await prisma.category.findMany();
